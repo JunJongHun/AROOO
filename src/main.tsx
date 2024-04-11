2d58928c54a35b7e42c532d3ed01733215592356
jonghun <minn4072@gmail.com>
2024-04-11 19:31:38 +0900

    chore: Chakra UI 설치 및 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router.tsx';
 import { worker } from './mocks/browsers.ts';
+import { ChakraProvider } from '@chakra-ui/react';
 
 if (import.meta.env.DEV) {
   await worker.start();
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <ChakraProvider>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </ChakraProvider>
+  //  </React.StrictMode>
 );
