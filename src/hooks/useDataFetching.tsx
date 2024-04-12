75897c9b099dae47ff402b591a8f22fb6cc4517a
jonghun <minn4072@gmail.com>
2024-04-12 15:48:37 +0900

    feat: 데이터 관리를 위한 useDataFetching 훅 추가 (무한로딩 발생)

---
+++ src/hooks/useDataFetching.tsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
+
+type ReturnType<T> = {
+  isLoading: boolean;
+  res: T | null;
+  isError: AxiosError | null;
+};
+
+type HttpMethod = 'GET' | 'POST' | 'PUT' | 'DELETE' | string;
+
+const useDataFetching = <T,>(
+  url: string,
+  data: Record<string, unknown> = {},
+  method: HttpMethod = 'GET'
+): ReturnType<T> => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [res, setRes] = useState<T | null>(null);
+  const [isError, setIsError] = useState<AxiosError | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const config: AxiosRequestConfig = {
+          method: method,
+          url: url,
+          data: data,
+        };
+
+        const response: AxiosResponse<T> = await axios(config);
+        setRes(response.data);
+      } catch (error) {
+        setIsError(error as AxiosError);
+      }
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [url, data, method]);
+
+  return { isLoading, res, isError };
+};
+
+export default useDataFetching;
