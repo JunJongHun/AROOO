aaa11c5e44104bbb74f7e81092302a7c01ef24fd
jonghun <minn4072@gmail.com>
2024-04-14 19:55:18 +0900

    feat: Header icon에 home으로 이동하는 기능 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,23 @@
 import { Box, Flex, Image, Text } from '@chakra-ui/react';
 import aroooPng from '../assets/arooo.png';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleMoveToHome = () => {
+    navigate('/');
+  };
+
   return (
     <Flex alignItems={'center'} justifyContent={'space-between'}>
-      <Image boxSize={70} objectFit="cover" src={aroooPng} alt="arooo icon" />
+      <Image
+        boxSize={70}
+        objectFit="cover"
+        src={aroooPng}
+        alt="arooo icon"
+        onClick={handleMoveToHome}
+      />
       <Text fontSize="xxx-large" color="black" fontWeight={400}>
         AROOO
       </Text>
