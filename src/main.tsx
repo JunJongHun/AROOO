7376e92e530f20ab40efb047c2d85062e8ed9cbb
jonghun <minn4072@gmail.com>
2024-04-20 19:53:06 +0900

    refact: strict 모드 적용

--- src/main.tsx
+++ src/main.tsx
@@ -6,18 +6,19 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { queryClient } from './queryClient.ts';
+import React from 'react';
 
 if (import.meta.env.DEV) {
   await worker.start();
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  // <React.StrictMode>
-  <ChakraProvider>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
-  </ChakraProvider>
-  //  </React.StrictMode>
+  <React.StrictMode>
+    <ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </ChakraProvider>
+  </React.StrictMode>
 );
